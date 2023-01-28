36371cb7c73c6a4f7329f4ac78ebce2f330c4874
phanuwat0 <mostpanuwat48@gmail.com>
2023-01-28 10:48:41 +0700

    V4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-nav-toggle");
-navBarToggle.addEventListener("click", function() {
+navBarToggle.addEventListener("click", function () {
   mainNav.classList.toggle('active');
 });
